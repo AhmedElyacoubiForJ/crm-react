65ba0b537f853b9328ba760a950339805664bae8
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2025-02-04 22:25:06 +0100

    refactor: Integrate StatusMessage component in AddEmployee component - Refactored AddEmployee component to use the StatusMessage component for handling loading and error states. - Improved code readability and consistency by using a reusable status message component.

--- src/components/pages/AddEmployee.jsx
+++ src/components/pages/AddEmployee.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { Card, Form, Button } from "react-bootstrap";
 import useFetchData from "../common/hooks/useFetchData";
 import { addEmployee, getDepartments } from "../../api/employees";
+import StatusMessage from "../common/StatusMessage";
 
 const AddEmployee = () => {
   const navigate = useNavigate();
@@ -66,106 +67,108 @@ const AddEmployee = () => {
           <h1>Mitarbeiter hinzufügen</h1>
         </Card.Header>
         <Card.Body>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="firstName">
-              <Form.Label>Vorname</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Vorname"
-                name="firstName"
-                value={employee.firstName}
-                onChange={handleInputChange}
-                isInvalid={!!validationErrors.firstName}
-              />
-              <Form.Control.Feedback type="invalid">
-                {validationErrors.firstName}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="lastName" className="mt-3">
-              <Form.Label>Nachname</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Nachname"
-                name="lastName"
-                value={employee.lastName}
-                onChange={handleInputChange}
-                isInvalid={!!validationErrors.lastName}
-              />
-              <Form.Control.Feedback type="invalid">
-                {validationErrors.lastName}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="email" className="mt-3">
-              <Form.Label>E-Mail</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="E-Mail"
-                name="email"
-                value={employee.email}
-                onChange={handleInputChange}
-                isInvalid={!!validationErrors.email}
-              />
-              <Form.Control.Feedback type="invalid">
-                {validationErrors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="department" className="mt-3">
-              <Form.Label>Abteilung</Form.Label>
-              {loading ? (
-                <p>Lade Abteilungen...</p>
-              ) : error ? (
-                <p className="text-danger">{error}</p>
-              ) : (
-                <>
-                  <Form.Control
-                    as="select"
-                    name="department"
-                    value={customDepartment ? "custom" : employee.department}
-                    onChange={handleInputChange}
-                    isInvalid={!!validationErrors.department}
-                  >
-                    <option value="">Wählen Sie eine Abteilung</option>
-                    {departments.map((dept, index) => (
-                      <option key={index} value={dept}>
-                        {dept}
-                      </option>
-                    ))}
-                    <option value="custom">Andere Abteilung...</option>
-                  </Form.Control>
-                  {customDepartment && (
+          <StatusMessage loading={loading} error={error}>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="firstName">
+                <Form.Label>Vorname</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Vorname"
+                  name="firstName"
+                  value={employee.firstName}
+                  onChange={handleInputChange}
+                  isInvalid={!!validationErrors.firstName}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {validationErrors.firstName}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="lastName" className="mt-3">
+                <Form.Label>Nachname</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Nachname"
+                  name="lastName"
+                  value={employee.lastName}
+                  onChange={handleInputChange}
+                  isInvalid={!!validationErrors.lastName}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {validationErrors.lastName}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="email" className="mt-3">
+                <Form.Label>E-Mail</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="E-Mail"
+                  name="email"
+                  value={employee.email}
+                  onChange={handleInputChange}
+                  isInvalid={!!validationErrors.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {validationErrors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="department" className="mt-3">
+                <Form.Label>Abteilung</Form.Label>
+                {departments ? (
+                  <>
                     <Form.Control
-                      type="text"
-                      placeholder="Geben Sie eine neue Abteilung ein"
-                      name="customDepartment"
-                      value={employee.department}
-                      onChange={handleCustomDepartmentChange}
-                      isInvalid={!!validationErrors.customDepartment}
-                      className="mt-2"
-                    />
-                  )}
-                </>
-              )}
-              <Form.Control.Feedback type="invalid">
-                {validationErrors.department ||
-                  validationErrors.customDepartment}
-              </Form.Control.Feedback>
-            </Form.Group>
+                      as="select"
+                      name="department"
+                      value={customDepartment ? "custom" : employee.department}
+                      onChange={handleInputChange}
+                      isInvalid={!!validationErrors.department}
+                    >
+                      <option value="">Wählen Sie eine Abteilung</option>
+                      {departments.map((dept, index) => (
+                        <option key={index} value={dept}>
+                          {dept}
+                        </option>
+                      ))}
+                      <option value="custom">Andere Abteilung...</option>
+                    </Form.Control>
+                    {customDepartment && (
+                      <Form.Control
+                        type="text"
+                        placeholder="Geben Sie eine neue Abteilung ein"
+                        name="customDepartment"
+                        value={employee.department}
+                        onChange={handleCustomDepartmentChange}
+                        isInvalid={!!validationErrors.customDepartment}
+                        className="mt-2"
+                      />
+                    )}
+                  </>
+                ) : (
+                  <p className="text-danger">
+                    Abteilungen konnten nicht geladen werden.
+                  </p>
+                )}
+                <Form.Control.Feedback type="invalid">
+                  {validationErrors.department ||
+                    validationErrors.customDepartment}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-            <div className="flex-container">
-              <Button
-                variant="primary"
-                type="submit"
-                className="mt-3 uniform-button"
-              >
-                Speichern
-              </Button>
-              <Link to="/view-employees" className="mt-3 ms-3">
-                <Button className="uniform-button" variant="secondary">
-                  Zurück zur Liste
+              <div className="flex-container">
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="mt-3 uniform-button"
+                >
+                  Speichern
                 </Button>
-              </Link>
-            </div>
-          </Form>
+                <Link to="/view-employees" className="mt-3 ms-3">
+                  <Button className="uniform-button" variant="secondary">
+                    Zurück zur Liste
+                  </Button>
+                </Link>
+              </div>
+            </Form>
+          </StatusMessage>
         </Card.Body>
       </Card>
     </div>
